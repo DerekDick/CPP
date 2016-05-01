2bd5742cb46558a1b689f68e0fcd6d18f01ad1d1
Hou Jianfeng <derekwangyi@163.com>
2016-05-01 15:33:02 +0800

    Revision
    Disable namespace std and replace the goto block.

--- Zhu-Hongming/Homework5/3_Maze.cpp
+++ Zhu-Hongming/Homework5/3_Maze.cpp
@@ -1,134 +1,118 @@
 #include <iostream>
 
-using namespace std;
-
 int n, m, k;
 char matrix[500][500];
 int tag[500][500];
-int Count = 0; //Count the number of the remaining empty cells(here use Count instead of count to avoid contradictation with std::count)
-int length; //The length of the stretching road
+int count = 0; //Count the number of the remaining empty cells
+int emptyNum = 0, remainedNum; //The number of original empyt cells and remained empty cells
 void stretch(int i, int j);
 
 int main(void) {
 	//Input
-	cin >> n >> m >> k;
+	std::cin >> n >> m >> k;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> matrix[i][j];
-		}
-	}
-
-	//Revising the matrix
-	int emptyNum = 0; //The number of the original empty cells
-	for (int i = 0; i < n; i++) { //Count the number of the empty cells
-		for (int j = 0; j < m; j++) {
+			std::cin >> matrix[i][j];
+			//Count the number of the original empty cells
 			if (matrix[i][j] == '.') {
 				emptyNum++;
 			}
 		}
 	}
-	int remainedNum = emptyNum - k; //The number of the remained empty cells
-	length = remainedNum;
+
+	//Tag the matrix
+	remainedNum = emptyNum - k; //The number of the remained empty cells
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			if (matrix[i][j] == '.') {
-				//Tag the remaining empty cells
 				stretch(i, j);
-				//Revising...
-				for (int i = 0; i < n; i++) {
-					for (int j = 0; j < m; j++) {
-						if (matrix[i][j] == '.' && tag[i][j] != 1) {
-							matrix[i][j] = 'X';
-						}
-					}
-				}
-				goto endLoop;
 			}
 		}
 	}
-endLoop:
-	;
+	
+	//Revise the matrix
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (matrix[i][j] == '.' && tag[i][j] != 1) {
+				matrix[i][j] = 'X';
+			}
+		}
+	}
 
 	//Output the revised matrix
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cout << matrix[i][j];
+			std::cout << matrix[i][j];
 		}
-		cout << endl;
+		std::cout << std::endl;
 	}
 
 	return 0;
 }
 
 void stretch(int i, int j) {
-	if (Count == length) {
+	if (count == remainedNum) {
 		return;
 	}
-
-	Count++;
+	count++;
 	tag[i][j] = 1;
+	
+	//Find the last position and set its tag to 1
+	char last = '\0';
+	if (i != 0) {
+		if (tag[i - 1][j] == -1) {
+			last = 'u';
+			tag[i - 1][j] = 1;
+		}
+	}
+	if (j != m - 1) {
+		if (tag[i][j + 1] == -1) {
+			last = 'r';
+			tag[i][j + 1] = 1;
+		}
+	}
+	if (i != n - 1) {
+		if (tag[i + 1][j] == -1) {
+			last = 'd';
+			tag[i + 1][j] = 1;
+		}
+	}
+	if (j != 0) {
+		if (tag[i][j - 1] == -1) {
+			last = 'l';
+			tag[i][j - 1] = 1;
+		}
+	}
 
 	//Stretch up
 	if (i != 0) {
-		if (matrix[i - 1][j] == '.') {
+		if (matrix[i - 1][j] == '.' && last != 'u' && tag[i - 1][j] != 1) {
 			tag[i][j] = -1;
-			if (tag[i - 1][j] == 1) {
-				;
-			}
-			else if (tag[i - 1][j] == -1) {
-				tag[i - 1][j] = 1;
-			}
-			else {
-				stretch(i - 1, j);
-			}
+			stretch(i - 1, j);
 		}
 	}
-
+	
 	//Stretch right
 	if (j != m - 1) {
-		if (matrix[i][j + 1] == '.') {
+		if (matrix[i][j + 1] == '.' && last != 'r' && tag[i][j + 1] != 1) {
 			tag[i][j] = -1;
-			if (tag[i][j + 1] == 1) {
-				;
-			}
-			else if (tag[i][j + 1] == -1) {
-				tag[i][j + 1] = 1;
-			}
-			else {
-				stretch(i, j + 1);
-			}
+			stretch(i, j + 1);
 		}
 	}
-
+	
 	//Stretch down
 	if (i != n - 1) {
-		if (matrix[i + 1][j] == '.') {
+		if (matrix[i + 1][j] == '.' && last != 'd' && tag[i + 1][j] != 1) {
 			tag[i][j] = -1;
-			if (tag[i + 1][j] == 1) {
-				;
-			}
-			else if (tag[i + 1][j] == -1) {
-				tag[i + 1][j] = 1;
-			}
-			else {
-				stretch(i + 1, j);
-			}
+			stretch(i + 1, j);
 		}
 	}
 
 	//Stretch left
 	if (j != 0) {
-		if (matrix[i][j - 1] == '.') {
+		if (matrix[i][j - 1] == '.' && last != 'l' && tag[i][j - 1] != 1) {
 			tag[i][j] = -1;
-			if (tag[i][j - 1] == 1) {
-				;
-			}
-			else if (tag[i][j - 1] == -1) {
-				tag[i][j - 1] = 1;
-			}
-			else {
-				stretch(i, j - 1);
-			}
+			stretch(i, j - 1);
 		}
 	}
 
